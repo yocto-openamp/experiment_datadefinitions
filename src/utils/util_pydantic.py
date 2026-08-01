e7626bc50d392183eafa385b7c21ea4d984c2459
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2026-08-01 13:47:37 +0200

    ModelHierarchy

--- src/utils/util_pydantic.py
+++ src/utils/util_pydantic.py
@@ -4,7 +4,10 @@ import dataclasses
 import logging
 import typing
 
+import annotated_types
 import pydantic
+
+from utils import util_pydantic
 
 logger = logging.getLogger(__file__)
 
@@ -50,3 +53,47 @@ class SchemaExtra:
     @property
     def dict(self) -> dict[str, typing.Any]:
         return dataclasses.asdict(self)
+
+
+class ModelHierarchy(dict[str, util_pydantic.FieldInfoIter]):
+    @staticmethod
+    def factory(model: pydantic.BaseModel) -> ModelHierarchy:
+        mh = ModelHierarchy()
+
+        def walk_model_paths(model: pydantic.BaseModel, prefix: str = "") -> None:
+            assert issubclass(model, pydantic.BaseModel)
+
+            for field in util_pydantic.FieldInfoIter.iter_model(model):
+                path = f"{prefix}/{field.field_name}"
+                annotation = field.field_info.annotation
+                if (annotation is list) or (typing.get_origin(annotation) is list):
+
+                    def get_annotation[T: annotated_types.BaseMetadata](
+                        annotated_type: type[T],
+                    ) -> T:
+                        for j in field.field_info.metadata:
+                            if isinstance(j, annotated_type):
+                                return j
+                        raise NotImplementedError()
+
+                    min_length = get_annotation(annotated_types.MinLen).min_length
+                    max_length = get_annotation(annotated_types.MaxLen).max_length
+                    assert min_length == max_length
+                    (item_type,) = typing.get_args(
+                        annotation
+                    )  # -> datamodel.unittests_simple.ModelPidController
+                    for i in range(min_length):
+                        walk_model_paths(item_type, f"{path}/{i}")
+                    continue
+                if issubclass(field.field_info.annotation, pydantic.BaseModel):
+                    walk_model_paths(field.field_info.annotation, path)
+                    continue
+                mh[path] = field
+
+        walk_model_paths(model=model)
+
+        return mh
+
+    def dump(self):
+        for path, item in self.items():
+            print(f"{path}: ...")
